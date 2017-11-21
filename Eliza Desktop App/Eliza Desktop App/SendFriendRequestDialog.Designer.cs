120c7fff300c6dc9d39610c8d1d9d5298eaf654b
David Harabagiu <harabagiudavid@gmail.com>
2017-11-22 00:28:18 +0200

    Updated icon for Send Friend Request Dialog

--- Eliza Desktop App/Eliza Desktop App/SendFriendRequestDialog.Designer.cs
+++ Eliza Desktop App/Eliza Desktop App/SendFriendRequestDialog.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SendFriendRequestDialog));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
@@ -48,6 +49,7 @@
             this.ClientSize = new System.Drawing.Size(334, 52);
             this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "SendFriendRequestDialog";
