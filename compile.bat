8fff8fb3a23fe295143e66ae2d7ca2f70e83d1a3
David Harabagiu <harabagiudavid@gmail.com>
2018-01-18 23:16:46 +0200

    Added batch script to compile the py source files and move them in the GUI directory

---
+++ compile.bat
@@ -0,0 +1,3 @@
+python -m compileall elizaclient.py guiconnection.py
+copy elizaclient.pyc "Eliza Desktop App\Eliza Desktop App\bin\Debug\elizaclient.pyc"
+copy guiconnection.pyc "Eliza Desktop App\Eliza Desktop App\bin\Debug\guiconnection.pyc"
